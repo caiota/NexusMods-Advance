7b157818215f43b12a3b1e4ce87cd4aa49f16fbb
Caio Menecuci <caiota.games@gmail.com>
2026-01-29 20:20:04 -0300

    Delete script directory

--- script/AutoFocusPostsSearch/AutoFocusPostsSearch.js
+++
@@ -1,7 +0,0 @@
-function FocusSearchElement(){
-    const formSearch=document.querySelector("input#comment_search");
-    if(formSearch){
-        formSearch.focus();
-        formSearch.click();
-    }
-}
